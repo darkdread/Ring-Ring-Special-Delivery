bbd960a5c252efdbdb7dcfe822aeef093914e645
Reynard <reynardcheng@gmail.com>
2019-12-14 17:59:31 +0800

    added camera pan

---
+++ Assets/Scripts/CameraPan.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour {
+
+	public static CameraPan instance;
+	public Transform posToMove;
+	// Start is called before the first frame update
+	void Awake() {
+		instance = this;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (posToMove != null) PanCamera();
+	}
+
+	public void PanCamera() {
+		while (transform.position != posToMove.position) {
+			transform.position = Vector3.Lerp(transform.position, posToMove.position, Time.deltaTime);
+			transform.rotation = Quaternion.Lerp(transform.rotation, posToMove.rotation, Time.deltaTime);
+			return;
+		}
+	}
+}
---
+++ Assets/Scripts/PanTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanTrigger : MonoBehaviour {
+	public Transform newCamPos;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			CameraPan.instance.posToMove = newCamPos;
+			CameraPan.instance.PanCamera();
+		}
+	}
+}
